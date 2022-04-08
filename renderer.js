cc469c36171274bc86c53f623c0abbfc2a30df70
Hoang Huy <hoangnx@mor.com.vn>
2022-04-08 07:45:02 +0700

    add crawl data

--- renderer.js
+++ renderer.js
@@ -9,22 +9,44 @@ const puppeteer = require("puppeteer");
 var page;
 var email;
 var password;
+var broswer;
 const login = async () => {
   email = document.getElementById("email").value;
   password = document.getElementById("password").value;
-  const browser = await puppeteer.launch({ headless: false });
+  browser = await puppeteer.launch({
+    headless: false,
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    userDataDir: "/tmp/myChromeSession",
+  });
   page = await browser.newPage();
   await page.goto("https://discord.com/login");
-  await page.type('[name="email"]', email);
-  await page.type('[name="password"]', password);
-  await page.click('[type="submit"]');
+  // await page.type('[name="email"]', email);
+  // await page.type('[name="password"]', password);
+  // await page.click('[type="submit"]');
 };
 const getListRoom = async () => {
-  const text = await page.evaluate(() => {
-    return document.querySelectorAll('[aria-label="Servers"]');
+  const listChannels = await page.evaluate(() => {
+    let channels = [];
+    let listChannel = document.querySelectorAll('[aria-label="Servers"]')[0];
+    listChannel.childNodes.forEach((channel, index) => {
+      channels.push(channel.innerHTML);
+    });
+
+    return channels;
   });
 
-  console.log(text);
+  const list = document.getElementById("list-channel");
+  listChannels.forEach((channel, index) => {
+    let ele = document.createElement("div");
+    ele.innerHTML = channel;
+    ele.setAttribute("data-index-channel", index);
+    ele.addEventListener("click", changeChannel);
+    list.appendChild(ele);
+  });
+};
+const changeChannel = async (e) => {
+  console.log(e);
 };
 // (async  () => {
 //   const browser = await puppeteer.launch({ headless: false });
