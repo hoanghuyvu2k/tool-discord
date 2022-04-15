0107cdc5db2b97b268ff1850268e33ca6d790489
Hoang Huy <hoangnx@mor.com.vn>
2022-04-15 08:04:01 +0700

    feat: auto comment

--- renderer.js
+++ renderer.js
@@ -11,8 +11,8 @@ var email;
 var password;
 var broswer;
 const login = async () => {
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
+  // email = document.getElementById("email").value;
+  // password = document.getElementById("password").value;
   browser = await puppeteer.launch({
     headless: false,
     executablePath:
@@ -20,39 +20,138 @@ const login = async () => {
     userDataDir: "/tmp/myChromeSession",
   });
   page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 600 });
   await page.goto("https://discord.com/login");
   // await page.type('[name="email"]', email);
   // await page.type('[name="password"]', password);
   // await page.click('[type="submit"]');
 };
 const getListRoom = async () => {
-  const listChannels = await page.evaluate(() => {
+  const listServers = await page.evaluate(() => {
+    let servers = [];
+    let listServer = document.querySelectorAll('[aria-label="Servers"]')[0];
+    listServer.childNodes.forEach((server, index) => {
+      servers.push(server.innerHTML);
+    });
+
+    return servers;
+  });
+
+  const list = document.getElementById("list-server");
+  for (let indexServer = 0; indexServer < listServers.length; indexServer++) {
+    let server = listServers[indexServer];
+    let ele = document.createElement("div");
+    let wrapperServer = document.createElement("div");
+    wrapperServer.innerHTML = server;
+    wrapperServer.setAttribute("class", "server border");
+    wrapperServer.addEventListener("click", function () {
+      changeServer(indexServer);
+    });
+    ele.setAttribute("class", "d-flex");
+    ele.appendChild(createInput(indexServer));
+    ele.appendChild(wrapperServer);
+    let channel = await getListChannel(indexServer);
+    ele.appendChild(channel);
+    list.appendChild(ele);
+  }
+};
+// const getListChannel = async (indexServer) => {
+//   await page.waitForSelector('[aria-label="Channels"]');
+//   const listChannelsFromWeb = await page.evaluate(() => {
+//     let channels = [];
+//     let listChannel = document.querySelectorAll('[aria-label="Channels"]')[0];
+//     listChannel.childNodes.forEach((channel, index) => {
+//       channels.push(channel.innerHTML);
+//     });
+
+//     return channels;
+//   });
+//   let listChannel = document.createElement("div");
+//   listChannel.setAttribute("id", `list-channel-${indexServer}`);
+//   listChannelsFromWeb.forEach((channel, indexChannel) => {
+//     let wrapperChannel = document.createElement("div");
+//     wrapperChannel.innerHTML = channel;
+//     wrapperChannel.setAttribute("class", "channel border");
+//     // wrapperChannel.addEventListener("click", function () {
+//     //   change(indexChannel);
+//     // });
+//     listChannel.appendChild(wrapperChannel);
+//   });
+//   console.log(listChannel);
+//   return listChannel;
+// };
+const getListChannel = async (indexServer) => {
+  // const list = document.getElementById("list-server");
+  // for (
+  //   let indexServer = 0;
+  //   indexServer < list.childNodes.length;
+  //   indexServer++
+  // ) {
+  changeServer(indexServer);
+  await page.waitFor(500);
+  await page.waitForSelector('[aria-label="Channels"]');
+  const listChannelsFromWeb = await page.evaluate(() => {
     let channels = [];
-    let listChannel = document.querySelectorAll('[aria-label="Servers"]')[0];
+    let listChannel = document.querySelectorAll('[aria-label="Channels"]')[0];
     listChannel.childNodes.forEach((channel, index) => {
-      channels.push(channel.innerHTML);
+      channels.push(channel.innerText);
     });
 
     return channels;
   });
-
-  const list = document.getElementById("list-channel");
-  listChannels.forEach((channel, index) => {
-    let ele = document.createElement("div");
-    ele.innerHTML = channel;
-    ele.setAttribute("data-index-channel", index);
-    ele.addEventListener("click", changeChannel);
-    list.appendChild(ele);
+  let listChannel = document.createElement("div");
+  listChannel.setAttribute("id", `list-channel-${indexServer}`);
+  listChannelsFromWeb.forEach((channel, indexChannel) => {
+    let wrapperChannel = document.createElement("div");
+    wrapperChannel.innerText = channel;
+    wrapperChannel.setAttribute("class", "channel border");
+    wrapperChannel.addEventListener("click", function () {
+      changeChannel(indexChannel);
+    });
+    listChannel.appendChild(wrapperChannel);
   });
+  return listChannel;
+  // list.childNodes[indexServer].appendChild(listChannel);
+  // console.log(listChannel);
+  // }
 };
-const changeChannel = async (e) => {
-  console.log(e);
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+const createInput = (index) => {
+  let input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = `server-${index}`;
+  input.setAttribute("class", "form-check-input");
+  return input;
 };
-// (async  () => {
-//   const browser = await puppeteer.launch({ headless: false });
-//   const page = await browser.newPage();
-//   await page.goto("https://example.com");
-//   await page.screenshot({ path: "example.png" });
-//   console.log("da chay");
-//   await browser.close();
-// })();
+const changeServer = async (index) => {
+  await page.click(`[aria-label="Servers"] >div:nth-child(${index + 1})`);
+};
+const changeChannel = async (index) => {
+  await page.click(`[aria-label="Channels"] :nth-child(${index + 1})`);
+};
+
+const comment = async () => {
+  // let comment = document.getElementById("comment").value;
+  // await page.type(".form-3gdLxP", comment);
+  // await page.keyboard.press("Enter");
+  document.querySelector(".toast-body").innerText = "Comment thanh cong!";
+  $(".toast").toast("show");
+};
+const waitForComment = async () => {
+  await page
+    .waitForSelector(
+      ".channelTextArea-1FufC0:not(.channelTextAreaDisabled-1p2fQv)",
+      { timeout: 10000 }
+    )
+    .then(() => {
+      comment();
+      $(".toast").toast("show");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
